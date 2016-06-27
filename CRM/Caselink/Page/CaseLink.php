78c47d3201a2fd3ec344715b3b8456739bb37eb8
Jaap Jansma <jaap@edeveloper.nl>
2016-06-27 15:50:44 +0200

    add case type to drop down

--- CRM/Caselink/Page/CaseLink.php
+++ CRM/Caselink/Page/CaseLink.php
@@ -23,16 +23,12 @@ class CRM_Caselink_Page_CaseLink extends CRM_Core_Page {
     $case_status = array();
     $params =array('name' => 'case_status');
     CRM_Core_BAO_OptionGroup::retrieve($params, $case_status);
-    $case_type = array();
-    $params =array('name' => 'case_type');
-    CRM_Core_BAO_OptionGroup::retrieve($params, $case_type);
-    $sql = "SELECT civicrm_case.*, civicrm_case_contact.contact_id as client_id, civicrm_contact.display_name, ov.label as case_status_label, ov2.label as case_type_label "
+    $sql = "SELECT civicrm_case.*, civicrm_case_contact.contact_id as client_id, civicrm_contact.display_name, ov.label as case_status_label"
       . "FROM `".$config->getCustomGroup('table_name')."` AS `case_link`
         INNER JOIN `civicrm_case` ON `case_link`.`".$config->getCaseIdField('column_name')."` = `civicrm_case`.`id`"
       . "INNER JOIN `civicrm_case_contact` ON `civicrm_case`.`id` = `civicrm_case_contact`.`case_id`"
       . "INNER JOIN `civicrm_contact` ON `civicrm_case_contact`.`contact_id`  = `civicrm_contact`.`id`"
       . "LEFT JOIN  civicrm_option_value ov ON ( civicrm_case.status_id=ov.value AND ov.option_group_id='".$case_status['id']."')"
-      . "LEFT JOIN  civicrm_option_value ov2 ON ( civicrm_case.case_type_id=ov2.value AND ov2.option_group_id='".$case_type['id']."')"
       . "WHERE `case_link`.`entity_id` = '".$this->caseId."' LIMIT 1";
     $dao = CRM_Core_DAO::executeQuery($sql);
     $case = false;
