24edb3fbf3c91ef708010d5991884f439d773e36
Jaap Jansma <jaap@edeveloper.nl>
2017-10-25 16:12:53 +0200

    Fixed werkmetzin issue #2069: hide related cases on certain case types.

--- CRM/Caselink/Page/Cases.php
+++ CRM/Caselink/Page/Cases.php
@@ -11,6 +11,10 @@ class CRM_Caselink_Page_Cases extends CRM_Core_Page {
     parent::__construct();
     $this->caseId = $caseId;
   }
+	
+	public function getCaseId() {
+		return $this->caseId;
+	}
 
   public function run() {
     $this->preProcess();
