2649baedcf25ed89453dc53081e70ec7a756cd24
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2023-10-05 13:58:40 +0200

    Executed civix update: motivation remove {0} problem

--- caselink.php
+++ caselink.php
@@ -106,17 +106,6 @@ function caselink_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function caselink_civicrm_xmlMenu(&$files) {
-  _caselink_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -169,54 +158,6 @@ function caselink_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 }
 
 /**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function caselink_civicrm_managed(&$entities) {
-  _caselink_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function caselink_civicrm_caseTypes(&$caseTypes) {
-  _caselink_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function caselink_civicrm_angularModules(&$angularModules) {
-_caselink_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function caselink_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _caselink_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
  * Functions below this ship commented out. Uncomment as required.
  *
 
@@ -225,8 +166,23 @@ function caselink_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function caselink_civicrm_preProcess($formName, &$form) {
-
-}
 
 */
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function caselink_civicrm_postInstall() {
+  _caselink_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function caselink_civicrm_entityTypes(&$entityTypes) {
+  _caselink_civix_civicrm_entityTypes($entityTypes);
+}
